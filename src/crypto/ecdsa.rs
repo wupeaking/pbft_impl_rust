16c6a503919763fe051e44dd4953394b71711849
wupeaking <wupeaking@gmail.com>
2021-09-09 23:26:03 +0800

    add

--- src/crypto/ecdsa.rs
+++ src/crypto/ecdsa.rs
@@ -1,11 +1,18 @@
 use crate::error::CounchError;
+use ring::{rand, signature};
 
 
 /// ## 随机生成一对公钥 私钥 
 /// 返回值为 私钥 公钥 错误 
-fn generate_key_pairs() -> Result<(String, String), CounchError> {
+fn generate_key_pairs() -> Result<(String, String), CounchError >{
+    let rng = rand::SystemRandom::new();
+    let key_pair = signature::EcdsaKeyPair::generate_pkcs8(&signature::ECDSA_P256_SHA256_FIXED_SIGNING,
+         &rng)?;
+
     if true {
         return Ok(("".to_string(), "".to_string()));
     }
 	return Err(CounchError::CryptoError("xxxx".to_string()));
 }
+
+// 
--- src/error.rs
+++ src/error.rs
@@ -1,15 +1,25 @@
 use thiserror::Error;
 use std::io;
-// use rocksdb::Error;
+use ring::error;
 
 #[derive(Error, Debug)]
 pub enum CounchError {
     #[error("IO连接错误 {0}")]
     IOError(#[from] io::Error),
+
     #[error("帧解析错误 {0}")]
     FrameParse(String),
-    #[error("数据库操作错误 {0}")]
+
+    #[error("加密错误 {0}")]
     CryptoError(String),
+
+    #[error("调用Ring加密模块出错")]
+    CryptoRingErr,
+
     #[error("未知错误 {0}")]
     Unknown(String),
 }
+
+ impl From<ring::error::Unspecified> for CounchError {
+     fn from(_: ring::error::Unspecified) -> Self { CounchError::CryptoRingErr }
+ }
