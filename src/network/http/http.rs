2c1f90304407e5208a2dca452713257c659e272c
pengxin.wu <wupeaking@gmail.com>
2021-09-13 18:38:57 +0800

    add

--- src/network/http/http.rs
+++ src/network/http/http.rs
@@ -1,3 +1,5 @@
+use std::net::SocketAddr;
+
 use crate::error::CounchError;
 use crate::network::{BroadcastMsg, OnReceive, Peer, SwitcherI};
 use log::{debug, error, info, log_enabled, Level};
@@ -34,11 +36,21 @@ impl HTTPNetWork {
         }
     }
 
-    pub async fn start() {
+    pub async fn start(&self) {
         //todo:: 启动服务
-        let hello = warp::path!("hello" / String).map(|name| format!("Hello, {}!", name));
-
-        warp::serve(hello).run(([127, 0, 0, 1], 3030)).await;
+        // POST /employees/:rate  {"name":"Sean","rate":2}
+        let promote = warp::post()
+            .and(warp::path("employees"))
+            .and(warp::path::param::<u32>())
+            // Only accept bodies smaller than 16kb...
+            .and(warp::body::content_length_limit(1024 * 16))
+            .and(warp::body::json())
+            .map(|rate, mut employee: Employee| {
+                employee.rate = rate;
+                warp::reply::json(&employee)
+            });
+        let addr = self.local_address.parse::<SocketAddr>().unwrap();
+        warp::serve(hello).run(addr).await;
     }
 }
 
